6f6c4d01a4d46d09a1d49951f8c50fa93dfd0e08
Chandler Craig <hellochandlercraig@gmail.com>
2022-02-16 11:59:12 -0800

    Done.

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map( phrase => {
+    let words = phrase.split(' ')
+    let capitalizedWords = words.map( word => word.charAt(0).toUpperCase() + word.slice(1) )
+    let response = capitalizedWords.join(' ')
+    return response
+  });
 }
